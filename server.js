9b5a877e461f0388d1636f5abe5547275265c63f
Julicode07 <Julicode07@gmail.com>
2025-03-29 17:58:51 -0500

    chore: update development URL environment variable for local development

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 5000;
 connectDB();
 
 // Middlewares
-app.use(cors());
+app.use(cors({ origin: "*" }));
 app.use(express.json());
 app.use("/uploads", express.static(path.join(__dirname, "src/uploads")));
 
